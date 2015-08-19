2dd19e30f90193b1331903ad98511aa425a86401
Andrew Carter <andrewcarter1992@gmail.com>
2015-08-19 01:33:53 +0100

    Added shutdown exception to allow the daemon to be able to distinguish between clean shutdowns and faults.

---
+++ src/Exception/ShutdownException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPFastCGI\FastCGIDaemon\Exception;
+
+/**
+ * Thrown when AFTER the daemon has been shutdown cleanly. This is used to
+ * cleanly distinguish between other faults that could occur during operation.
+ */
+class ShutdownException extends \RuntimeException
+{
+}
