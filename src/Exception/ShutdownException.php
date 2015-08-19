915ef2d22119af8a29fffadd069b89bcf30da52f
Andrew Carter <andrewcarter1992@gmail.com>
2015-08-19 01:56:24 +0100

    PHPdoc error

--- src/Exception/ShutdownException.php
+++ src/Exception/ShutdownException.php
@@ -3,8 +3,8 @@
 namespace PHPFastCGI\FastCGIDaemon\Exception;
 
 /**
- * Thrown when AFTER the daemon has been shutdown cleanly. This is used to
- * cleanly distinguish between other faults that could occur during operation.
+ * Thrown AFTER the daemon has been shutdown cleanly. This is used to cleanly
+ * distinguish between other faults that could occur during operation.
  */
 class ShutdownException extends \RuntimeException
 {
